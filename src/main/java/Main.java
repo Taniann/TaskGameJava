747c07f462ab7b6738e2f74ba7b5913bdc2007fd
Таня <nebesnatania@outlook.com>
2018-07-07 21:54:33 +0300

    //

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,10 @@
  * Created by Таня on 07.07.2018.
  */
 public class Main {
+    public static void main(String[] args) {
+        Model model = new Model();
+        View view = new View();
+        Controller controller = new Controller(model, view);
+        controller.processUser();
+    }
 }
